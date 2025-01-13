d912704bf6a198bda0b047a245efff929a4f12a3
Magnemyr Martin <notch8719@gmail.com>
2025-01-13 17:51:35 +0100

    Updated scripts

---
+++ start/update.bat
@@ -0,0 +1,23 @@
+@echo off
+echo Martes Restore
+
+REM Load environment variables from .env file
+setlocal enabledelayedexpansion
+for /f "tokens=1,2 delims==" %%a in ('type .env') do (
+    if "%%a"=="MARTES_REMOTE_HOME" set "MARTES_REMOTE_HOME=%%b"
+)
+
+REM Kontrollera om variabeln är satt
+if "%MARTES_REMOTE_HOME%"=="" (
+    echo MARTES_REMOTE_HOME är inte satt. Kontrollera .env-filen.
+    pause
+    exit /b
+)
+
+REM Debug: Print the value of MARTES_REMOTE_HOME
+echo MARTES_REMOTE_HOME is set to: %MARTES_REMOTE_HOME%
+
+REM Run the backup script
+wsl sh "%MARTES_REMOTE_HOME%/update.sh"
+
+pause
