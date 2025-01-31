1d9b5a14d67914257beffcb4ed931f918669b868
Martin Magnemyr <martin.magnemyr@goteborgenergi.se>
2025-01-31 13:48:15 +0100

    te

--- start/check.bat
+++ start/check.bat
@@ -1,28 +1,11 @@
+
 @echo off
-echo Kontrollerar Martes Containers...
-
+chcp 65001 >nul
 set "ENV_FILE=%~dp0..\.env"
-
-REM Read .env file and extract variables
 for /f "tokens=1,2 delims==" %%a in ('findstr /r "^[^#]" "%ENV_FILE%"') do (
     if "%%a"=="MARTES_REMOTE_HOME" set "MARTES_REMOTE_HOME=%%b"
     if "%%a"=="DISTRO_NAME" set "DISTRO_NAME=%%b"
 )
 
-REM Load environment variables from .env file
-setlocal enabledelayedexpansion
-for /f "tokens=1,2 delims==" %%a in ('type .env') do (
-    if "%%a"=="MARTES_REMOTE_HOME" set "MARTES_REMOTE_HOME=%%b"
-)
-
-REM Kontrollera om variabeln är satt
-if "%MARTES_REMOTE_HOME%"=="" (
-    echo MARTES_REMOTE_HOME är inte satt. Kontrollera .env-filen.
-    pause
-    exit /b
-)
-
-REM Print the value of MARTES_REMOTE_HOME
-echo MARTES_REMOTE_HOME=%MARTES_REMOTE_HOME%
 echo wsl  -d %DISTRO_NAME% docker ps
 wsl  -d %DISTRO_NAME% docker ps
