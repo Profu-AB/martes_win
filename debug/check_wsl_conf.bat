8513d854b8d83018dae68d3052d4fbb2625ea698
Magnemyr Martin <notch8719@gmail.com>
2025-02-01 10:25:42 +0100

    added a debug folder

---
+++ debug/check_wsl_conf.bat
@@ -0,0 +1,9 @@
+@echo off
+chcp 65001 >nul
+set "ENV_FILE=%~dp0..\.env"
+for /f "tokens=1,2 delims==" %%a in ('findstr /r "^[^#]" "%ENV_FILE%"') do (
+    if "%%a"=="MARTES_REMOTE_HOME" set "MARTES_REMOTE_HOME=%%b"
+    if "%%a"=="DISTRO_NAME" set "DISTRO_NAME=%%b"
+)
+
+wsl -d %DISTRO_NAME% cat /etc/wsl.conf
