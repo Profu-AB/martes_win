b4805ea980a04781675d4ceb07a9d89bce50e337
Magnemyr Martin <notch8719@gmail.com>
2025-02-04 07:10:37 +0100

    added debug/netstat.bat

---
+++ debug/netstat.bat
@@ -0,0 +1,3 @@
+netstat -ano | findstr :8000
+
+netstat -ano | findstr :80
--- start/update.bat
+++ start/update.bat
@@ -1,5 +1,7 @@
 @echo off
 chcp 65001 >nul
+setlocal EnableDelayedExpansion
+
 set "ENV_FILE=%~dp0..\.env"
 for /f "tokens=1,2 delims==" %%a in ('findstr /r "^[^#]" "%ENV_FILE%"') do (
     if "%%a"=="MARTES_REMOTE_HOME" set "MARTES_REMOTE_HOME=%%b"
