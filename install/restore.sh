fcaad813a2cde107169da2eb8059dfaff6af9b6a
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 11:52:53 +0100

    dummy

--- install/restore.sh
+++ install/restore.sh
@@ -12,5 +12,5 @@ LINUX_PATH=$(echo $WIN_PATH | sed 's|\\|/|g' | sed 's|C:|/mnt/c|')
 echo "Restoring backup in : $LINUX_PATH/backup"
 
 docker exec martes_mongodb rm -rf /restore_backup
-docker cp  $LINUX_PATH/backup martes_mongodb:/restore_backup
+docker cp  ../backup martes_mongodb:/restore_backup
 docker exec -i martes_mongodb mongorestore --username admin --password secret --authenticationDatabase admin --drop --dir /restore_backup --nsExclude='admin.*'
