3720264c6f293ace34a945348741027664a4a625
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 12:16:30 +0100

    dummy

--- install/restore.sh
+++ install/restore.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <windows_path>"
-  exit 1
-fi
 
-WIN_PATH=$1
-LINUX_PATH=$(echo $WIN_PATH | sed 's|\\|/|g' | sed 's|C:|/mnt/c|')
-
-
-echo "Restoring backup in : $LINUX_PATH/backup"
+echo "Restoring backup in : backup"
 
 docker exec martes_mongodb rm -rf /restore_backup
 docker cp  ../backup martes_mongodb:/restore_backup
