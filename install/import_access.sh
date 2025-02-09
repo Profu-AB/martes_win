b7d0409952729e9a9dfa2322200280640cf099b5
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 12:48:21 +0100

    dummy

--- install/import_access.sh
+++ install/import_access.sh
@@ -1,31 +1,11 @@
+
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <windows_path>"
-  exit 1
-fi
+# Get the current directory (Linux path)
+CURRENT_PATH=$(pwd)
 
-WIN_PATH=$1
-LINUX_PATH=$(echo $WIN_PATH | sed 's|\\|/|g' | sed 's|C:|/mnt/c|')
-
-# Debug: Print the paths
-echo "WIN_PATH: $WIN_PATH"
-echo "LINUX_PATH: $LINUX_PATH"
-
-# rm -rf ./msaccess
-mkdir msaccess
-
-# Debug: Check if the LINUX_PATH exists
-if [ ! -d "$LINUX_PATH" ]; then
-  echo "Error: Directory $LINUX_PATH does not exist."
-  exit 1
-fi
-
-# Debug: List the contents of the LINUX_PATH
-echo "Contents of $LINUX_PATH:"
-ls -la "$LINUX_PATH"
-
-# cp -r "$LINUX_PATH"/* ./msaccess/
+# Print current directory and backup path
+echo "Reading access databases from: $CURRENT_PATH/msaccess"
 
 docker exec martes_backend rm -rf /msaccess
 docker cp ./msaccess martes_backend:/msaccess
