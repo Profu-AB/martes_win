584e95c5f744d889dbd5b90b457ebcc055287312
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 12:21:22 +0100

    dummy

--- install/restore.sh
+++ install/restore.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# Get the current directory (Linux path)
+CURRENT_PATH=$(pwd)
 
-echo "Restoring backup in : ../backup"
+# Print current directory and backup path
+echo "Restoring backup from: $CURRENT_PATH/../backup"
 
-echo docker exec martes_mongodb rm -rf /restore_backup
+# Remove the existing backup directory in the container
+echo "Removing existing /restore_backup in the container"
 docker exec martes_mongodb rm -rf /restore_backup
 
-echo docker cp  ../backup martes_mongodb:/restore_backup
-docker cp  ../backup martes_mongodb:/restore_backup
+# Copy the backup folder to the container
+echo "Copying backup to container"
+docker cp $CURRENT_PATH/../backup martes_mongodb:/restore_backup
 
-echo docker exec -i martes_mongodb mongorestore --username admin --password secret --authenticationDatabase admin --drop --dir /restore_backup --nsExclude='admin.*'
+# Run mongorestore command inside the container
+echo "Restoring backup in MongoDB"
 docker exec -i martes_mongodb mongorestore --username admin --password secret --authenticationDatabase admin --drop --dir /restore_backup --nsExclude='admin.*'
