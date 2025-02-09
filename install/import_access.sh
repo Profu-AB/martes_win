062f236e991687a4db7a25c56b8af4f94fbf27a7
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 12:51:48 +0100

    dummy

--- install/import_access.sh
+++ install/import_access.sh
@@ -7,7 +7,7 @@ CURRENT_PATH=$(pwd)
 # Print current directory and backup path
 echo "Reading access databases from: $CURRENT_PATH/msaccess"
 
-docker exec martes_backend rm -rf /msaccess
-docker cp ./msaccess martes_backend:/msaccess
+docker exec martes_backend rm -rf /app/msaccess
+docker cp ./msaccess martes_backend:/app/msaccess
 # Execute the import.sh script located in the /app folder of the container
 docker exec martes_backend sh -c "cd /app && ./import.sh"
