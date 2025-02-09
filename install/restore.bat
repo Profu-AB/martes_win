8fe60e898ae20c389f48298b82876a51378fd426
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 12:23:38 +0100

    dummy

--- install/restore.bat
+++ install/restore.bat
@@ -22,10 +22,15 @@ rem Convert the parent path to a WSL-compatible path using wslpath
 for /f "delims=" %%i in ('wsl wslpath "%PARENT_PATH%"') do set WSL_PARENT_PATH=%%i
 
 rem Ensure the restore.sh script is in the correct WSL path
-set RESTORE_PATH=%WSL_PARENT_PATH%/install/restore.sh
+set RESTORE_PATH=%WSL_PARENT_PATH%\restore.sh
+
+rem Define the correct path for the backup directory (assuming it's one level up)
+set BACKUP_PATH=%WSL_PARENT_PATH%\backup
+
+rem Debug: Print the value of BACKUP_PATH
+echo BACKUP_PATH is set to: %BACKUP_PATH%
 
 rem Run the restore script inside the WSL distribution
-echo "chmod +x '%RESTORE_PATH%' && '%RESTORE_PATH%'"
-wsl -d %DISTRO_NAME% --exec bash -c "chmod +x '%RESTORE_PATH%' && '%RESTORE_PATH%'"
+wsl -d %DISTRO_NAME% --exec bash -c "chmod +x '%RESTORE_PATH%' && '%RESTORE_PATH%' '%BACKUP_PATH%'"
 
 pause
