661ee1b2234965fc1202c64282855cc1d8e0e344
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 12:19:08 +0100

    dummy

--- install/restore.sh
+++ install/restore.sh
@@ -3,6 +3,11 @@
 
 echo "Restoring backup in : ../backup"
 
+echo docker exec martes_mongodb rm -rf /restore_backup
 docker exec martes_mongodb rm -rf /restore_backup
+
+echo docker cp  ../backup martes_mongodb:/restore_backup
 docker cp  ../backup martes_mongodb:/restore_backup
+
+echo docker exec -i martes_mongodb mongorestore --username admin --password secret --authenticationDatabase admin --drop --dir /restore_backup --nsExclude='admin.*'
 docker exec -i martes_mongodb mongorestore --username admin --password secret --authenticationDatabase admin --drop --dir /restore_backup --nsExclude='admin.*'
