3f1640ff2a2a49f9e51815dc1ea7bfe9d10224fc
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 15:32:45 +0100

    update.bat

--- install/update.sh
+++ install/update.sh
@@ -1,14 +1,18 @@
-git pull
+
+#!/bin/bash
+
+# Get the current directory (Linux path)
+CURRENT_PATH=$(pwd)
 
 # turn off the running containers
-docker compose -f docker-compose.yaml.old down
-docker compose -f docker-compose.yaml down
+docker compose -f ./install/docker-compose.yaml down
+
 
 # pull updates
-docker compose pull
+docker compose  -f ./install/docker-compose.yaml  pull
 
 # start conntainers
-docker compose up -d
+# docker compose  -f ./install/docker-compose.yaml up -d
 
 # remove any unused containers
 docker image prune -f
