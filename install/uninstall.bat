54b1a7b2574b63b267f87ddd6940ae705c5c41c2
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 10:37:17 +0100

    confirmation question in uninstall.bat

--- install/uninstall.bat
+++ install/uninstall.bat
@@ -1,10 +1,24 @@
 @echo off
 chcp 65001 >nul
 set "ENV_FILE=%~dp0..\.env"
+
+rem Read DISTRO_NAME from the .env file
 for /f "tokens=1,2 delims==" %%a in ('findstr /r "^[^#]" "%ENV_FILE%"') do (
-    
     if "%%a"=="DISTRO_NAME" set "DISTRO_NAME=%%b"
 )
 
-wsl --unregister %DISTRO_NAME%
-wsl --list -v
+rem Ask the user for confirmation
+echo Det här kommer avinstallera Martes och all data du har sparat kommer att försvinna (dock ej backuper). Är du säker på att du vill göra detta?
+choice /M "Press Y to confirm, N to cancel"
+
+rem If user presses "Y", continue with the unregistration
+if errorlevel 2 (
+    echo Operation canceled.
+    exit /B
+) else (
+    echo Unregistering the WSL distro "%DISTRO_NAME%"...
+    wsl --unregister %DISTRO_NAME%
+    wsl --list -v
+)
+
+rem End of script
