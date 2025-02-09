63d09d6f1958d81b5484937e982a1e72bbe36494
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 10:43:45 +0100

    updated

--- install/uninstall.bat
+++ install/uninstall.bat
@@ -9,7 +9,8 @@ for /f "tokens=1,2 delims==" %%a in ('findstr /r "^[^#]" "%ENV_FILE%"') do (
 
 rem Ask the user for confirmation
 echo Det här kommer avinstallera Martes och all data du har sparat kommer att försvinna (dock ej backuper). Är du säker på att du vill göra detta?
-choice /M "Tryck Y för att gå vidare, N för att avbryta"
+
+choice /C YN /M "Tryck Y för att gå vidare, N för att avbryta"
 
 rem If user presses "Y", continue with the unregistration
 if errorlevel 2 (
