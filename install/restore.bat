d45bd48fe9853c3a56451c17ad1d9e5bdbe70b07
Magnemyr Martin <notch8719@gmail.com>
2025-02-09 11:32:22 +0100

    r

--- install/restore.bat
+++ install/restore.bat
@@ -1,24 +1,27 @@
 @echo off
 chcp 65001 >nul
 set "ENV_FILE=%~dp0..\.env"
+
+rem Read DISTRO_NAME from .env file
 for /f "tokens=1,2 delims==" %%a in ('findstr /r "^[^#]" "%ENV_FILE%"') do (
-    
     if "%%a"=="DISTRO_NAME" set "DISTRO_NAME=%%b"
 )
 
-
-REM Get the current directory in Windows format
+rem Get the current directory in Windows format
 for /f "delims=" %%i in ('wsl wslpath -w "$(pwd)"') do set CURRENT_PATH=%%i
 
-REM Move up one directory
+rem Move up one directory and get the parent path
 set PARENT_PATH=%~dp0..
 cd %PARENT_PATH%
 set PARENT_PATH=%cd%
 
-REM Debug: Print the value of PARENT_PATH
+rem Debug: Print the value of PARENT_PATH
 echo PARENT_PATH is set to: %PARENT_PATH%
 
-REM Run the restore script
-wsl -d %DISTRO_NAME% -- bash -c "bash -c sh './restore.sh' '%PARENT_PATH%'"
+rem Convert the parent path to a WSL-compatible path using wslpath
+for /f "delims=" %%i in ('wsl wslpath "%PARENT_PATH%"') do set WSL_PARENT_PATH=%%i
+
+rem Run the restore script inside the WSL distribution
+wsl -d %DISTRO_NAME% bash -c "sh /mnt/c/%WSL_PARENT_PATH%/restore.sh"
 
 pause
