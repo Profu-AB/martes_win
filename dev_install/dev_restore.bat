42394eae64c6ab64c2a516d58e854c0002955f76
Magnemyr Martin <notch8719@gmail.com>
2025-02-12 14:47:18 +0100

    updated dev restore

--- dev_install/dev_restore.bat
+++ dev_install/dev_restore.bat
@@ -1,25 +1,36 @@
 @echo off
 chcp 65001 >nul
 set "ENV_FILE=%~dp0..\.env"
+
+rem Read DISTRO_NAME from .env file
 for /f "tokens=1,2 delims==" %%a in ('findstr /r "^[^#]" "%ENV_FILE%"') do (
-    
-    if "%%a"=="DISTRO_NAME" set "DISTRO_NAME=%%b"
+    if "%%a"=="DISTRO_DEFAULT_NAME" set "DISTRO_NAME=%%b"
 )
 
-
-
-
-REM Get the current directory in Windows format
+rem Get the current directory in Windows format
 for /f "delims=" %%i in ('wsl wslpath -w "$(pwd)"') do set CURRENT_PATH=%%i
 
-REM Move up one directory
+rem Move up one directory and get the parent path
 set PARENT_PATH=%~dp0..
 cd %PARENT_PATH%
 set PARENT_PATH=%cd%
 
-REM Debug: Print the value of PARENT_PATH
+rem Debug: Print the value of PARENT_PATH
 echo PARENT_PATH is set to: %PARENT_PATH%
 
-REM Run the restore script
-wsl  -d %DISTRO_NAME%  sh "./restore_dev.sh" "%PARENT_PATH%"
+rem Convert the parent path to a WSL-compatible path using wslpath
+for /f "delims=" %%i in ('wsl wslpath "%PARENT_PATH%"') do set WSL_PARENT_PATH=%%i
+
+rem Ensure the restore.sh script is in the correct WSL path
+set RESTORE_PATH=%WSL_PARENT_PATH%/dev_install/dev_restore.sh
+
+rem Define the correct path for the backup directory (assuming it's one level up)
+set BACKUP_PATH=%WSL_PARENT_PATH%/backup
+
+rem Debug: Print the value of BACKUP_PATH
+echo BACKUP_PATH is set to: %BACKUP_PATH%
+
+rem Run the restore script inside the WSL distribution
+wsl -d %DISTRO_NAME% --exec bash -c "chmod +x '%RESTORE_PATH%' && '%RESTORE_PATH%' '%BACKUP_PATH%'"
+
 pause
---
+++ dev_install/dev_restore.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the current directory (Linux path)
+CURRENT_PATH=$(pwd)
+
+# Print current directory and backup path
+echo "Restoring backup from: $CURRENT_PATH/backup"
+
+# Remove the existing backup directory in the container
+echo "Removing existing /restore_backup in the container"
+docker exec martes_mongodb_dev rm -rf /restore_backup
+
+# Copy the backup folder to the container
+echo "Copying backup to container"
+docker cp $CURRENT_PATH/backup martes_mongodb_dev:/restore_backup
+
+# Run mongorestore command inside the container
+echo "Restoring backup in MongoDB"
+docker exec -i martes_mongodb_dev mongorestore --username admin --password secret --authenticationDatabase admin --drop --dir /restore_backup --nsExclude='admin.*'
