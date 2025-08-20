908908668735a9185e15baf995d51bd3c1d085ee
Martin Magnemyr <martin.magnemyr@gmail.com>
2025-08-20 11:52:08 +0200

    Update install_wsl.bat
    Signed-off-by: Martin Magnemyr <martin.magnemyr@gmail.com>

--- dev_install/install_wsl.bat
+++ dev_install/install_wsl.bat
@@ -18,7 +18,7 @@ REM Combine sudo commands to avoid multiple password prompts
 wsl bash -c "sudo bash -c 'id -u martes &>/dev/null || adduser --disabled-password --gecos \"\" martes; echo \"%LINUX_USER% ALL=(martes) NOPASSWD: ALL\" > /etc/sudoers.d/%LINUX_USER%; echo \"martes ALL=(ALL) NOPASSWD: ALL\" > /etc/sudoers.d/martes; rm -rf /home/martes/martes_setup'"
 
 REM Clone the repository if it doesn't exist, otherwise pull the latest changes as martes user
-wsl bash -c "sudo -u martes bash -c 'if [ ! -d ~/martes_setup ]; then git clone https://github.com/mmagnemyr/martes_setup.git ~/martes_setup; else cd ~/martes_setup && git pull; fi'"
+wsl bash -c "sudo -u martes bash -c 'if [ ! -d ~/martes_setup ]; then git clone https://github.com/Profu-AB/martes_setup.git ~/martes_setup; else cd ~/martes_setup && git pull; fi'"
 
 REM Run setup.sh in the martes_setup folder as martes user
 wsl bash -c "sudo -u martes bash -c 'cd ~/martes_setup && bash setup_new.sh'"
