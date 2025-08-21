ff437f4c222aae8a30dd3672d6066986b7f31504
Magnemyr Martin <notch8719@gmail.com>
2025-08-21 23:06:10 +0200

    added readme.md

--- dev_install/bash_dev_wsl.bat
+++ dev_install/bash_dev_wsl.bat
@@ -1,1 +1,13 @@
-wsl -d Ubuntu-22.04 --exec bash -c "cd /home/martin/martes/docker/dev && exec bash"
+@echo off
+chcp 65001 >nul
+
+
+set "ENV_FILE=%~dp0..\.env"
+for /f "tokens=1,2 delims==" %%a in ('findstr /r "^[^#]" "%ENV_FILE%"') do (
+    
+    if "%%a"=="DISTRO_DEFAULT_NAME" set "DISTRO_NAME=%%b"
+    if "%%a"=="LINUX_USER_NAME" set "LINUX_USER_NAME=%%b"
+)
+
+
+wsl -d %DISTRO_DEFAULT_NAME% --exec bash -c "cd /home/%LINUX_USER_NAME%/martes/docker/dev && exec bash"
---
+++ dev_install/readme.md
@@ -0,0 +1,12 @@
+# Utvecklingsmiljö
+
+
+## 01. Uppdatera .env filen 
+
+DISTRO_NAME=profu-martes
+DISTRO_DEFAULT_NAME=Ubuntu-22.04
+LICENSE=[din licenskod för martes]
+LINUX_USER_NAME=[ditt linux user name som du skall använda i linux tex martin]
+
+
+install.bat
