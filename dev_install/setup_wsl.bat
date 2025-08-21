c7c2eeba8c6711fd69144e79e599195517a4239e
Magnemyr Martin <notch8719@gmail.com>
2025-08-21 22:53:21 +0200

    updated setup_wsl.bat

--- dev_install/setup_wsl.bat
+++ dev_install/setup_wsl.bat
@@ -6,6 +6,7 @@ set "ENV_FILE=%~dp0..\.env"
 for /f "tokens=1,2 delims==" %%a in ('findstr /r "^[^#]" "%ENV_FILE%"') do (
     
     if "%%a"=="DISTRO_DEFAULT_NAME" set "DISTRO_NAME=%%b"
+    if "%%a"=="LINUX_USER_NAME" set "LINUX_USER_NAME=%%b"
 )
 
 :: Get the current script directory dynamically
@@ -14,7 +15,7 @@ set "FOUND=false"
 
 
 
-wsl -d %DISTRO_NAME% bash -c "sudo bash -c 'id -u martes &>/dev/null || adduser --disabled-password --gecos \"\" martes; echo \"martin ALL=(martes) NOPASSWD: ALL\" > /etc/sudoers.d/martin; echo \"martes ALL=(ALL) NOPASSWD: ALL\" > /etc/sudoers.d/martes; rm -rf /home/martes/martes_setup'"
+wsl -d %DISTRO_NAME% bash -c "sudo bash -c 'id -u martes &>/dev/null || adduser --disabled-password --gecos \"\" martes; echo \"%LINUX_USER_NAME% ALL=(martes) NOPASSWD: ALL\" > /etc/sudoers.d/%LINUX_USER_NAME%; echo \"martes ALL=(ALL) NOPASSWD: ALL\" > /etc/sudoers.d/martes; rm -rf /home/martes/martes_setup'"
 
 REM Clone the repository if it doesn't exist, otherwise pull the latest changes as martes user
 wsl -d %DISTRO_NAME% bash -c "sudo -u martes bash -c 'if [ ! -d ~/martes_setup ]; then git clone https://github.com/Profu-AB/martes_setup.git ~/martes_setup; else cd ~/martes_setup && git pull; fi'"
